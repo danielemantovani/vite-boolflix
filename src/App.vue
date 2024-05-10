e6f9b3689787bdbe414e9c056c81eaa6f2f645c8
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-10 19:12:36 +0200

    feat: stelle

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,82 @@
 <script>
 
 import axios from "axios"
-import {store} from "./store.js"
+import { store } from "./store.js"
 
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
-export default{
+export default {
   components: {
     AppHeader,
     AppMain,
 
   },
-  data(){
-    return{
+  data() {
+    return {
       store,
     }
   },
 
-  methods:{
+  methods: {
+  
+    // getData(){
+
+    //   this.isLoading = true
+
+    //   const params = {
+    //     api_key: this.store.apiKey, 
+    //     query: this.store.searchQuery
+    //   }
+
+    //   const showMovie = axios.get ("https://api.themoviedb.org/3/search/movie",{
+    //     params,
+    //   });
+    //   const showTv = axios.get ("https://api.themoviedb.org/3/search/tv",{
+    //     params,
+    //   });
+
+    //   axios.all ([showMovie, showTv]). then ((resp) => {
+    //     this.store.moviesArray = resp[0].data.results;
+    //     this.store.tvArray = resp[1].data.results;
+    //   })
+    //   //finally sarà eseguito quando arriveranno tutte due le riposte
+    //   .finally (()=>{
+    //     this.isLoading = false
+    //   });
+    // },
+
+    getMovie() {
+      this.showMovie(),
+      this.showTv()
+    },
+
     //funzione per fare chiamata api per la categoria film
-    showMovie(){
+    showMovie() {
       console.log("click");
       axios
-        .get ("https://api.themoviedb.org/3/search/movie",{
+        .get("https://api.themoviedb.org/3/search/movie", {
           params: {
             api_key: this.store.apiKey, //chiave personale api importata dallo store 
             query: this.store.searchQuery, //come da documentazione dell'api che stiamo utilizzando query è la parola chiave per filtrare i titoli dei film, quindi quando andremo a digitare qualcosa nell'input del component AppHeader ci usciranno i risultati inerenti alla parola cercata
           }
         })
-        .then((resp) =>{
+        .then((resp) => {
           // console.log(resp);
           this.store.moviesArray = resp.data.results; //inseriamo nell'array vuoto nello store la risposta che ci da la chiamata api contenente l'elenco di tutti i film presenti
           console.log(this.store.moviesArray);
         });
     },
 
-    showTV(){
+    showTv() {
       axios
-        .get ("https://api.themoviedb.org/3/search/tv",{
+        .get("https://api.themoviedb.org/3/search/tv", {
           params: {
             api_key: this.store.apiKey,
             query: this.store.searchQuery,
           }
         })
-        .then((resp) =>{
+        .then((resp) => {
           this.store.tvArray = resp.data.results;
           console.log(this.store.tvArray);
         });
@@ -55,14 +87,9 @@ export default{
 
 <template>
   <!-- gli passiamo la parola all'interno delle parentesi nell'emit (in AppHeader) per far recepire al genitore che ciò che succede al figlio  -->
-  <AppHeader 
-    @filter= "[showMovie(), showTV()]"
-  />
-  <AppMain 
-  />
+  <AppHeader @filter="getMovie" />
+  <AppMain />
 
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,16 +8,25 @@ export default {
             flagArray: ["en", "es", "fr", "it", "ja", "pt"],
         }
     },
-    methods:{
+    computed: {
+        // se c'è title facciamo cardObj.title altrimenti facciamo cardObj.name
+        cardTitle() {
+            return this.cardObj.title ? this.cardObj.title : this.cardObj.name
+        },
+        cardOriginalTitle() {
+            return this.cardObj.original_title ? this.cardObj.original_title : this.cardObj.original_name
+        }
+    },
+    methods: {
         //funzione di default per gestire le immagini con path dinamico dove con name indichiamo il nome del file della bandiera presente nella cartella assets, img
-        getImageUrl(name){
-            return new URL ('../assets/img/' + name + '.png', import.meta.url).href; //concatenazione
+        getImageUrl(name) {
+            return new URL('../assets/img/' + name + '.png', import.meta.url).href; //concatenazione
             // return new URL (`../assets/img/${name}.png`, import.meta.url).href; template literals
         },
-        getflags (){
-            if (this.flagArray.includes (this.cardObj.original_language)){
+        getflags() {
+            if (this.flagArray.includes(this.cardObj.original_language)) {
                 return true
-            } else{
+            } else {
                 return false
             }
         }
@@ -28,32 +37,30 @@ export default {
 <template>
     <div class="card">
         <div class="card-body text-center">
-            <img  :src="`https://image.tmdb.org/t/p/w342/${cardObj.poster_path}`" :alt=(cardObj.title)>
-            <!-- titolo film -->
-            <h3 v-if="cardObj.title">{{ cardObj.title }}</h3>
-            <!-- titolo serie tv -->
-            <h3 v-else="cardObj.name">{{ cardObj.name }}</h3>
-            <!-- titolo originale film -->
-            <h6 v-if="cardObj.original_title">{{ cardObj.original_title }}</h6>
-            <!-- titolo originale serie tv -->
-            <h6 v-else="cardObj.original_name">{{ cardObj.original_name }}</h6>
+            <img :src="`https://image.tmdb.org/t/p/w342/${cardObj.poster_path}`" :alt=(cardObj.title)>
+            <!-- <h3>{{ cardObj.title ? cardObj.title : cardObj.name }}</h3> -->
+            <h3>{{ cardTitle }}</h3>
+            <h6>{{ cardOriginalTitle }}</h6>
             <!-- mostra l'icona della bandiera in base alla lingua se la bandiera è presente nell'array e quindi c'è l'immagine corrispondente -->
-            <img class="flag" v-if="getflags()"
-            :src="getImageUrl(cardObj.original_language)" 
-            :alt= "(cardObj.original_language)"
-            >
+            <img class="flag" v-if="getflags()" :src="getImageUrl(cardObj.original_language)"
+                :alt="(cardObj.original_language)">
             <!-- altrimenti stampa la lingua in lettere -->
             <p v-else>{{ cardObj.original_language }}</p>
             <p>{{ cardObj.vote_average }}</p>
+            <!-- stelle -->
+            <i class="fa-star" v-for="star in stars" :class="{
+                fas: star <= Math.ceil(cardObj.vote_average / 2),
+                far: star > Math.ceil(cardObj.vote_average / 2),
+            }"></i>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-    .card{
-        .flag{
-            width: 30px;
-            height: 20px;
-        }
+.card {
+    .flag {
+        width: 30px;
+        height: 20px;
     }
+}
 </style>
