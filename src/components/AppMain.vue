26c6477b9df72b22459e0d898438a103ba25359b
danielemantovani <daniele.mantovani124@gmail.com>
2024-09-06 16:34:42 +0200

    feat: title in AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,12 +19,14 @@ export default {
 </script>
 
 <template>
-    <div class="bg-dark mt-5">
+    <div class="bg-dark">
+        <h2 class="text-white text-center mt-5 p-5 fs-1">FILM</h2>
         <section class="movies row">
-            <div class="container col g-3 p-5" v-for="film in store.moviesArray"> 
+            <div class="container col-4 g-3 p-5" v-for="film in store.moviesArray"> 
                 <AppCard :cardObj="film" />
             </div>
         </section>
+        <h2 class="text-white text-center p-5 fs-1" >SERIE TV</h2>
         <section class="tv-series row">
             <div class="container col g-3 p-5" v-for="tv in store.tvArray">
                 <AppCard :cardObj ="tv"/>
